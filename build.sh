31fe71b6d87b3cd5253a7675f8fb198eece39e4f
Robert <rmetcalf9@googlemail.com>
2023-11-02 17:31:20 +0000

    Updating build

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@
 source ./_repo_vars.sh
 echo "Building project ${PROJECT_NAME}"
 
-docker build --build-arg="PM_VERSION=4.1.21" . -t ${BUILD_IMAGE_NAME_AND_TAG}
+TAG=${BUILD_IMAGE_NAME_AND_TAG}
+if [$# -ne 0];
+  echo "Tag provided: ${TAG}"
+  exit 1
+fi
+
+docker build --build-arg="PM_VERSION=4.1.21" . -t ${TAG}
+
+exit 0
