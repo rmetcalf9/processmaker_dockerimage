e163dc7ea169d183fedf4a2d95796c2b68fbdf53
Robert <rmetcalf9@googlemail.com>
2023-11-27 11:09:19 +0000

    Using mariadb to correct issue

--- _repo_vars.sh
+++ _repo_vars.sh
@@ -4,4 +4,7 @@ export PROJECT_NAME=${PWD##*/}          # to assign to a variable
 export PROJECT_NAME=${PROJECT_NAME:-/}
 export BUILD_IMAGE_NAME_AND_TAG=${PROJECT_NAME}_build_image:local
 
+export MARIADB_IMAGE_FOR_CLIENT=mariadb:10.9.8
+export TESTING_DB_PASS=testingmysqldb_examplepassword
+
 source ./statefiles/inp_env_file
--- init.sh
+++ init.sh
@@ -4,8 +4,14 @@ source ./_repo_vars.sh
 echo "Running database init process for project ${PROJECT_NAME}"
 
 
-docker exec -i dev_machine_utilities_dmu-processmakermysqldb.1.nej79426mumtshfvp54f1b3dz mysql -pprocessmakermysqldb_examplepassword < ./_init_setup_database.sql
+#docker exec -i dev_machine_utilities_dmu-processmakermysqldb.1.nej79426mumtshfvp54f1b3dz mysql -pprocessmakermysqldb_examplepassword < ./_init_setup_database.sql
 
+docker run --rm -i --network=main_net --name=processmaker_init_db ${MARIADB_IMAGE_FOR_CLIENT} mysql -h "${PM_DB_HOST}" -P "${PM_DB_PORT}" -p${TESTING_DB_PASS} < ./_init_setup_database.sql
+RES=$?
+if [[ ${RES} -ne 0 ]]; then
+  echo "Docker command to init db failed"
+  exit ${RES}
+fi
 
 CUR_DIR=$(pwd)
 
