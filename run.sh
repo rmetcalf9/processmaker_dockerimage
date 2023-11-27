dd7453376ab954c387df3f93e2ac2f21fbc1c40d
Robert <rmetcalf9@googlemail.com>
2023-11-27 14:08:41 +0000

    Script Execution Working

--- run.sh
+++ run.sh
@@ -13,7 +13,6 @@ docker run --rm -it --name ${PROJECT_NAME}_run \
  -v "${CUR_DIR}/statefiles:/statefiles" \
  -e "ENVFILE=/statefiles/inp_env_file" \
  -p ${PM_APP_PORT}:80 \
- -p 6001:6001 \
  ${BUILD_IMAGE_NAME_AND_TAG}
 
 exit 0
